17633119b73ad12fe9d304ab6dbe68e2c0225d55
Julia <150614653+MysteriousJulyMoon@users.noreply.github.com>
2025-05-28 10:49:31 +0200

    Delete protocols directory

--- protocols/user.py
+++
@@ -1,8 +0,0 @@
-from typing import Protocol
-
-from .data import DataRepositoryProtocol
-from data.user import User
-
-
-class UserRepositoryProtocol(DataRepositoryProtocol[User], Protocol):
-    def get_by_login(self, login: str) -> User | None: ...
